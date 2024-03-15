3d3c4c53f76b9059533d299f5950490d9a5617e1
DerPipo636 <schumacherphilipp9@gmail.com>
2024-03-15 19:17:34 +0100

    Good Night

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ menu.addEventListener('click', function(event){
     if (clickTarget.classList.contains('nav-link')){
         clickTarget.classList.add('active');
         activeBtn.classList.remove('active')
+        let mobile = document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')){
+            mobile.classList.add('hide')
+        }
+    }
+    let body = document.querySelector("body");
+    let mobileMenu = document.querySelector(".mobile-menu");
+    if (!body.classList.contains("off-scroll") &&
+    !mobileMenu.classList.contains('hide')){
+        body.classList.add("off-scroll");
+    }
+    else{
+        body.classList.remove("off-scroll");
     }
 });
 let classLink = '.main-link';
@@ -30,5 +43,10 @@ let activeBtn = document.querySelector('.active');
 let newActiveBtn = document.querySelector(classLink);
 if (!newActiveBtn.classList.contains('active')){
     newActiveBtn.classList.add('active');
-    activeBtn.classList.remove('active')
-};
+    activeBtn.classList.remove('active');
+}
+document.querySelector(".mobile-button").addEventListener("click",
+function(event)
+{
+    document.querySelector("mobile-menu").classList.toggle("hide");
+    });
